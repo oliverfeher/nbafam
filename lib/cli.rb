9617824fd95d0cb1bb961c2ba7aab3379a3bb6e8
oliverfeher <oliver92.feher@gmail.com>
2020-02-09 00:02:45 -0500

    Add class CLI
    - Add method,  brings up welcome section
    - Nes list_options for available selections

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,2 +1,36 @@
 require "pry"
 
+class Cli
+
+    def run
+        puts "                                                            
+NNNNNNNN        NNNNNNNNBBBBBBBBBBBBBBBBB               AAA               
+N:::::::N       N::::::NB::::::::::::::::B             A:::A              
+N::::::::N      N::::::NB::::::BBBBBB:::::B           A:::::A             
+N:::::::::N     N::::::NBB:::::B     B:::::B         A:::::::A            
+N::::::::::N    N::::::N  B::::B     B:::::B        A:::::::::A           
+N:::::::::::N   N::::::N  B::::B     B:::::B       A:::::A:::::A          
+N:::::::N::::N  N::::::N  B::::BBBBBB:::::B       A:::::A A:::::A         
+N::::::N N::::N N::::::N  B:::::::::::::BB       A:::::A   A:::::A        
+N::::::N  N::::N:::::::N  B::::BBBBBB:::::B     A:::::A     A:::::A       
+N::::::N   N:::::::::::N  B::::B     B:::::B   A:::::AAAAAAAAA:::::A      
+N::::::N    N::::::::::N  B::::B     B:::::B  A:::::::::::::::::::::A     
+N::::::N     N:::::::::N  B::::B     B:::::B A:::::AAAAAAAAAAAAA:::::A    
+N::::::N      N::::::::NBB:::::BBBBBB::::::BA:::::A             A:::::A   
+N::::::N       N:::::::NB:::::::::::::::::BA:::::A               A:::::A  
+N::::::N        N::::::NB::::::::::::::::BA:::::A                 A:::::A 
+NNNNNNNN         NNNNNNNBBBBBBBBBBBBBBBBBAAAAAAA                   AAAAAAA
+"
+        puts "---------------------------------------------------------------------------"
+        puts "                    Welcome to the NBA family!"
+        puts ""
+        puts ""
+        list_options
+    end
+    
+    
+    
+
+
+        
+end
